786035a4061535c88c7113d4a269268c8dc1df00
1amSumit <sj779619@gmail.com>
2023-11-08 17:58:28 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+import 'package:services/screens/places_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+final colorSheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 102, 6, 247),
+  background: const Color.fromARGB(255, 56, 49, 66),
+  brightness: Brightness.dark,
+);
+
+final theme = ThemeData().copyWith(
+  useMaterial3: true,
+  scaffoldBackgroundColor: colorSheme.background,
+  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+    titleSmall: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+    titleMedium: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+    titleLarge: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Great Places",
+      home: PlacesScreen(),
+    );
+  }
+}
---
+++ lib/screens/places_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PlacesScreen extends StatefulWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  State<PlacesScreen> createState() => _PlacesScreenState();
+}
+
+class _PlacesScreenState extends State<PlacesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Your Places"),
+        ),
+      ),
+    );
+  }
+}
